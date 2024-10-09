193f8f6801cc0b280d2c039720e03b688c0cbdfe
Brayan <brayanhuincho975@gmail.com>
2024-10-08 23:13:30 -0500

    Añadir proyecto de calculadora

---
+++ script.js
@@ -0,0 +1,58 @@
+let parentesisAbierto = true;
+
+function agregarNumero(numero) {
+    const pantalla = document.getElementById('pantalla');
+    pantalla.value += numero;
+}
+
+function agregar(caracter) {
+    const pantalla = document.getElementById('pantalla');
+    pantalla.value += caracter;
+}
+
+function limpiar() {
+    document.getElementById('pantalla').value = '';
+}
+
+function operar(operador) {
+    const pantalla = document.getElementById('pantalla');
+    pantalla.value += ' ' + operador + ' ';
+}
+
+function cambiarSigno() {
+    const pantalla = document.getElementById('pantalla');
+    if (pantalla.value) {
+        pantalla.value = String(-parseFloat(pantalla.value));
+    }
+}
+
+function alternarParentesis() {
+    const pantalla = document.getElementById('pantalla');
+    if (parentesisAbierto) {
+        pantalla.value += '(';
+    } else {
+        pantalla.value += ')';
+    }
+    parentesisAbierto = !parentesisAbierto;
+}
+
+function borrar() {
+    const pantalla = document.getElementById('pantalla');
+    if (pantalla.value) {
+        pantalla.value = pantalla.value.slice(0, -1); 
+    }
+}
+
+function calcular() {
+    const pantalla = document.getElementById('pantalla');
+    try {
+        const expresion = pantalla.value
+            .replace(/×/g, '*') // Sustituir el símbolo de multiplicación
+            .replace(/÷/g, '/') // Sustituir el símbolo de división
+            .replace(/(\d+)\s*\(\s*/g, '$1*('); // Multiplicar el número antes de un paréntesis
+        pantalla.value = eval(expresion);
+    } catch (error) {
+        pantalla.value = 'Error'; // Mostrar 'Error' si hay un problema
+    }
+}
+
